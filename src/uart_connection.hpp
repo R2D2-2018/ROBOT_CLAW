ed03d91b12096dc1824fdd0d361990f9b346f639
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-05-17 21:35:30 +0200

    Documentated methods and member values

--- src/uart_connection.hpp
+++ src/uart_connection.hpp
@@ -22,41 +22,142 @@ public:
 
   UARTConnection(unsigned int baudrate, UARTController controller = UARTController::ONE, bool initializeController = true);
 
+  /**
+   * @brief Begin a UART connection.
+   * 
+   * This method initializes the selected USART (universal synchronous and asynchronous receiver-transmitter) controller located
+   * on the Arduino Due. By default, this method is called at object construction, but this can be disabled.
+   * 
+   */
   void begin();
-  //bool available();
+
+  /**
+   * @brief Check how many bytes are available to read.
+   * 
+   * By checking the size of the receive buffer, we determine if new data has arrived.
+   * 
+   * @return unsigned int Amount of bytes available to read.
+   */
   unsigned int available();
+
+  /**
+   * @brief Enables the internal USART controller.
+   * 
+   */
   inline void enable();
+
+  /**
+   * @brief Disables the internal USART controller.
+   * 
+   */
   inline void disable();
 
+  /**
+   * @brief Send a single byte.
+   * 
+   * @param c Byte.
+   * @return true Byte send.
+   * @return false Byte has not been send, USART controller not initialized.
+   */
   bool send(const char c);
+
+  /**
+   * @brief Send a string.
+   * 
+   * @param str String.
+   * @return true String send.
+   * @return false String has not been send, USART controller not initialized.
+   */
   bool send(const char *str);
+
+  /**
+   * @brief Send a array of bytes with a specified length.
+   * 
+   * @param data Array of bytes.
+   * @param length Length of array.
+   * @return true Array of bytes send.
+   * @return false Array of bytes has not been send, USART controller not initialized.
+   */
   bool send(const char* data, size_t length);
 
+  /**
+   * @brief Receive a single byte.
+   * 
+   * By popping the first element of the receive buffer, we return a received byte in a FIFO manner.
+   * 
+   * @return char Received byte.
+   */
   char receive();
-  //const char* receiveString();
 
+  /**
+   * @brief Checks if the internal USART controller has been initialized.
+   * 
+   * @return true Internal USART controller is initialized.
+   * @return false Internal USART has not been initialized. See the begin() method.
+   */
   bool isInitialized();
 
+  /**
+   * @brief Send a single byte.
+   * 
+   * @param c Byte.
+   */
   void operator<<(const char c);
+
+  /**
+   * @brief Send a string.
+   * 
+   * @param str String (must be null terminated).
+   */
   void operator<<(const char *str);
 
+  /**
+   * @brief Receive a byte.
+   * 
+   * @param c Byte.
+   */
   void operator>>(char &c);
 
   ~UARTConnection();
 
 private:
+  /**
+   * @brief Pointer the access the internal USART controllers.
+   * 
+   */
   Usart *hardwareUSART = nullptr;
-  unsigned int baudrate;
-  UARTController controller;
-  //Usart *hardwareUSARTTwo = USART1;
-  //Usart *hardwareUSARTThree = USART2;
 
+  /**
+   * @brief Data baudrate used for sending and receiving.
+   * 
+   */
+  unsigned int baudrate;
+
+  /**
+   * @brief Enumerable type used to select on of the three USART controllers located on the Arduino Due. 
+   * 
+   */
+  UARTController controller;
+
+  /**
+   * @brief Holds the initialization status of the USART controller.
+   * 
+   */
   bool USARTControllerInitialized = false;
 
-  //char rxBuffer[800];
-  Queue<char, 800> rxBuffer;
-  int rxBufferIndex = 0, rxReadIndex = 0;
+  /**
+   * @brief UART receive buffer.
+   * 
+   */
+  Queue<char, 250> rxBuffer;
+  //int rxBufferIndex = 0, rxReadIndex = 0;
 
+  /**
+   * @brief Checks if the USART controller reports that the transmitter is ready to send.
+   * 
+   * @return true Ready to send.
+   * @return false Not ready to send.
+   */
   inline bool txReady();
 
   /**
@@ -66,6 +167,11 @@ private:
    */
   void sendByte(const char &b);
 
+  /**
+   * @brief Receive a single byte by reading the US_RHR register.
+   * 
+   * @return char 
+   */
   inline char receiveByte();
 };
 
