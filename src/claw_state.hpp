7d1eaae589dbd42e2022564b487e884f6ea32e30
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-05-30 14:11:42 +0200

    Created ClawState enum

---
+++ src/claw_state.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @brief     Interface for the uArm Swift Pro claw/gripper
+ * @author    Wiebe van Breukelen, Sam Zandee
+ * @license   See LICENSE
+ */
+#ifndef CLAW_STATE_HPP
+#define CLAW_STATE_HPP
+
+enum class ClawState {
+    STOPPED, MOVING, GRIPPED_OBJECT, UNKNOWN
+};
+
+#endif
